69db7a55550f96cf73ef5bf5b9dcc89249a01a25
hrmsk66 <hrmsk66@gmail.com>
2022-05-27 08:29:58 +0900

    fix temp var name in for_each

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 )
 
 var cfgFile string
-var version = "0.1.9"
+var version = "0.2.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
